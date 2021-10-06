5d3d594fd5d276312cf5f5a3009d48ab0caf54ac
AlexHamn <agalexandro16@gmail.com>
2021-10-06 14:33:45 -0500

    create new game using the API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    name: "My cool new game" 
+  })
+})
+  .then(res => {
+    return res.json()
+  })
+  .then(data => console.log(data));
